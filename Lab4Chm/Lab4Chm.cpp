9803a0f5997317a5f7ae456ead5b089ee0e58ea5
Анастасия Лузинсан <luzinsan@mail.ru>
2022-04-19 15:19:52 +0700

    Add project files.

---
+++ Lab4Chm/Lab4Chm.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include "NonlinearEquations.h"
+
+using namespace luMath;
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    NonlinearEquations<double> data;
+    switch (data.getMethod())
+    {
+    case NonlinearEquations<double>::METHOD::NEWTON:  // Метод Ньютона
+        data.Newton();
+        break;
+    case NonlinearEquations<double>::METHOD::ITERATIONS: // Метод Итераций
+        data.Iterations();
+        break;
+    case NonlinearEquations<double>::METHOD::STEEPESTDESCENT: // Метод наискорейшего спуска
+        data.SteepestDescent();
+        break;
+    }
+    return 0;
+}
---
+++ Lab4Chm/NonlinearEquations.h
@@ -0,0 +1,73 @@
+#pragma once
+#include <fstream>
+#include <string>
+#include "../../../2/Lab2Chm/Lab2Chm/Vector.h"
+
+namespace luMath
+{
+	template<class T>
+	class NonlinearEquations
+	{
+    public:
+        enum class METHOD
+        {
+            NEWTON=1,
+            ITERATIONS,
+            STEEPESTDESCENT,
+        };
+    private:
+        METHOD _method;
+        int                 n;      // Размерность СНУ
+        T                   x0;     // Начальное приближение
+        T                   eps;    // Требуемая погрешность решения
+        Vector<char*>*       FunSys; // Система функций
+        std::ofstream      _fout;   // Выходной файл
+    public:
+
+        NonlinearEquations() 
+        {
+            std::ifstream _fin("input.txt");
+            _fout = std::ofstream("output.txt");
+            int method;
+            _fin >> method >> n >> x0 >> eps;
+            _method = static_cast<METHOD>(method);
+           
+            const char* str = "111";
+            FunSys = new Vector<char*>(n, str, true);
+           
+            
+            //for (int i = 0; i < n; i++)
+            //   FunSys[i] = " ";
+                
+        
+            std::cout << FunSys << '\n';
+            _fin.close();
+        }
+        ~NonlinearEquations()
+        {
+            delete FunSys;
+            _fout.close();
+        }
+
+        METHOD getMethod() { return _method; }
+
+        void Newton() 
+        {
+        
+        
+        }
+        void Iterations() 
+        {
+        
+        
+        }
+        void SteepestDescent() 
+        {
+        
+        
+        
+        }
+
+	};
+}
+
