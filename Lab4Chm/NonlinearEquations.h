97d57f91a7e2625d4cdbc425f3a97ff50b01d389
Анастасия Лузинсан <luzinsan@mail.ru>
2022-05-01 21:20:02 +0700

    нахождение обраной матрицы Якоби

--- Lab4Chm/NonlinearEquations.h
+++ Lab4Chm/NonlinearEquations.h
@@ -3,6 +3,7 @@
 #include <string>
 #include "../../../2/Lab2Chm/Lab2Chm/Matrix.h"
 #include "../../../2/Lab2Chm/Lab2Chm/Vector.h"
+
 #include "PolStr.h"
 
 namespace luMath
@@ -41,9 +42,6 @@ namespace luMath
                 x0[i] = c;
             }
             _fin >> eps;
-            
-           
-            
             FunSys = Vector<std::string>(n);
             
             _fin.seekg(2, std::ios_base::cur);
@@ -60,6 +58,76 @@ namespace luMath
         }
 
         METHOD getMethod() { return _method; }
+
+        static Vector<T> GaussMethod(const Matrix<T>& A, const Vector<T>& b)
+        {
+            Matrix<T> expandedMatrix(A.getRows(), A.getCols() + 1);
+            for (int i = 0; i < expandedMatrix.getRows(); i++)
+                for (int j = 0; j < expandedMatrix.getCols(); j++)
+                    if (j == expandedMatrix.getCols() - 1)
+                        expandedMatrix[i][j] = b[i];
+                    else
+                        expandedMatrix[i][j] = A[i][j];
+            return GaussMethod(expandedMatrix);
+        }
+
+        static Vector<T> GaussMethod(const Matrix<T>& expandedMatrix)
+        {
+            Matrix<T> tempMatrix(expandedMatrix);
+           
+            for (int i = 0; i < tempMatrix.getRows(); i++) 
+            {
+                T coeff = tempMatrix[i][i]; 
+                
+
+                for (int j = i; j < tempMatrix.getRows() + 1; j++)
+                    tempMatrix[i][j] /= coeff;
+                
+                for (int j = i + 1; j < tempMatrix.getRows(); j++)
+                {
+                    coeff = tempMatrix[j][i]; 
+                    for (int k = i; k < tempMatrix.getCols(); k++) 
+                        tempMatrix[j][k] -= coeff * tempMatrix[i][k]; 
+                }
+            }
+
+            Vector<T> result(tempMatrix.getRows());
+            result.transposition();
+            
+            for (int i = result.getLength() - 1; i >= 0; i--)
+            {
+                T sumCoeff = 0;
+                for (int j = i + 1; j < result.getLength(); j++)
+                    sumCoeff += tempMatrix[i][j] * result[j];
+
+                result[i] = tempMatrix[i][result.getLength()] - sumCoeff;
+            }
+            return result;
+        }
+
+        static Matrix<T> getInverseMatrixByMethod(Vector<T>(*Method)(const Matrix<T>&, const Vector<T>&), Matrix<T> matrix)
+        {
+            int m = matrix.getCols();
+            Matrix<T> inverseMatrix(m);
+            Vector<Vector<T>> x_temp(m);
+            Vector<Vector<T>> E(m);
+            for (int i = 0; i < m; i++)
+            {
+                E[i] = Vector<T>(m);
+                E[i][i] = 1;
+                E[i].transposition();
+            }
+
+            for (int i = 0; i < m; i++)
+                x_temp[i] = Method(matrix, E[i]);
+                
+            
+            for (int i = 0; i < m; i++)
+                for (int j = 0; j < m; j++)
+                    inverseMatrix[i][j] = x_temp[j][i];
+            std::cout << std::setw(10) << inverseMatrix;
+            return inverseMatrix;
+        }
 
         Matrix<T> getJacobi(Vector<T> x) 
         {
@@ -88,8 +156,8 @@ namespace luMath
 
         void Newton() 
         {
-            
             Matrix<T> J = getJacobi(x0);
+            Matrix<T> J_1 = getInverseMatrixByMethod(NonlinearEquations::GaussMethod, J);
         
         }
         void Iterations() 
